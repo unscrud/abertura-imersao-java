9e2363f1619a7518248caf56b01a10a3df533753
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-15 20:21:01 -0300

    chamada simples de uma api

--- src/App.java
+++ src/App.java
@@ -1,10 +1,20 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // acessar url da API
+        var url = "https://imdb-api.com/en/API/Top250Movies/chaveDaAPI";
+        URI uri = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(uri).build();
 
         // pegar o JSON
+        String json = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
 
         // imprimir o JSON
-        System.out.println("Hello, World!");
+        System.out.println(json);
     }
 }
