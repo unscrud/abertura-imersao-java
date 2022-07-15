4031b97e34e4e929074d9eb7c239e99b8c1404a1
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-15 19:48:29 -0300

    TODO

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        // acessar url da API
+
+        // pegar o JSON
+
+        // imprimir o JSON
         System.out.println("Hello, World!");
     }
 }
