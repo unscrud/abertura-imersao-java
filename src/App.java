b7a4f871820dff27dc7fb0801889ee1b50ae9c4a
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-21 20:59:00 -0300

    refatoração da logica de conexão

--- src/App.java
+++ src/App.java
@@ -1,9 +1,5 @@
 import java.io.InputStream;
-import java.net.URI;
 import java.net.URL;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
@@ -13,26 +9,22 @@ public class App {
         // acessar url da API
         // var url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
         var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&start_date=2022-06-12&end_date=2022-06-14";
-        URI uri = URI.create(url);
-        var client = HttpClient.newHttpClient();
-        var request = HttpRequest.newBuilder(uri).build();
 
         // pegar o JSON
-        String json = client.send(request, HttpResponse.BodyHandlers.ofString()).body();
+        String json = ClienteHttp.buscaDados(url);
 
         // extrair so os dados que interessam
-        var parser = new JsonParser();
-        List<Map<String, String>> listaDeFilmes = parser.parse(json);
+        List<Map<String, String>> listaDeConteudos = JsonParser.parse(json);
 
         // exibir e manipular os dados
         GeradorDeFigurinhas gerador = new GeradorDeFigurinhas();
         for (int i = 0; i < 10; i++) {
-            Map<String, String> filme = listaDeFilmes.get(i);
+            Map<String, String> conteudo = listaDeConteudos.get(i);
             String urlImagem =
                     // filme.get("image")
-                    filme.get("url")
+                    conteudo.get("url")
                             .replaceAll("(@+)(.*).jpg$", "$1.jpg");
-            String titulo = filme.get("title");
+            String titulo = conteudo.get("title");
             InputStream inputStream = new URL(urlImagem).openStream();
             String tituloSimples = titulo.replaceAll("[^a-zZ-Z1-9]", "");
             if (tituloSimples.isEmpty()) {
