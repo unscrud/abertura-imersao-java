64ffc1aed30f7d74473218a01cd16c685712dbc7
Daniel Carvalho <daniel.devop@gmail.com>
2022-07-21 08:23:27 -0300

    geração de figuras

--- src/App.java
+++ src/App.java
@@ -1,4 +1,6 @@
+import java.io.InputStream;
 import java.net.URI;
+import java.net.URL;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
@@ -22,11 +24,14 @@ public class App {
         List<Map<String, String>> listaDeFilmes = parser.parse(json);
 
         // exibir e manipular os dados
+        GeradorDeFigurinhas gerador = new GeradorDeFigurinhas();
         for (Map<String, String> filme : listaDeFilmes) {
-            System.out.println(filme.get("title"));
-            System.out.println(filme.get("image"));
-            System.out.println(filme.get("imDbRating"));
-            System.out.println();
+            String urlImagem = filme.get("image");
+            String titulo = filme.get("title");
+            InputStream inputStream = new URL(urlImagem).openStream();
+            gerador.cria(inputStream, titulo + ".png");
+
+            System.out.println(titulo);
         }
     }
 }
